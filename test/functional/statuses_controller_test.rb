7dc8cad5816f644cc0124bfe6edaee3c72f698af
Garan Guillory <garanguillory@gmail.com>
2014-06-20 11:15:41 +0200

    Scoped status updates to the current user on create and update.

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -36,6 +36,20 @@ class StatusesControllerTest < ActionController::TestCase
       post :create, status: { content: @status.content}
     end
 
+      assert_redirected_to status_path(assigns(:status))
+  end
+
+  test "should create status for the current user when logged in" do
+    sign_in users(:Garan)
+
+    assert_difference('Status.count') do
+      post :create, status: { content: @status.content, user_id: users(:jim).id}
+  end
+
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:Garan).id
+  end
+
   test "should show status" do
     get :show, id: @status
     assert_response :success
